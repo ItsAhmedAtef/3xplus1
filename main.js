8cff7999c7e8f0169d99d9db096af7e706d38e9f
Ahmed Atef <65677356+ItsAhmedAtef@users.noreply.github.com>
2022-10-05 19:13:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,239 +1,126 @@
-/***** ***** Global variables ***** *****/
-var submit = document.getElementById("submit");
-var input = document.getElementById("input");
-var output = document.getElementById("output");
-var quote = document.getElementById("quote");
-var hint = document.getElementById("hint");
-var title = document.getElementById("title");
-var tooltips = document.querySelectorAll('.show');
-var mode = document.getElementById("mode"); // id
-var MODE = localStorage.getItem('mode'); // browser storage value
-var chartparent = document.getElementById("chartparent");
-var all = [];
-var labelsall = [];
-var xx = '';
-var myChart = null;
-var scrollButton = document.getElementById("scrollButton");
-/***** ***** !Global variables ***** *****/
+const side = document.getElementById("side");
+const items = side.querySelectorAll(".item");
+const theme = document.getElementById("theme");
 
-function showlogs() {
-    var logsdiv = output.querySelector("#logs");
-    var logsbutton = output.querySelector("#showlogs");    
-    if (logsdiv) {
-        if (logsdiv.style.display == "none") {
-            logsdiv.style.display = "block";
-            logsbutton.innerHTML = "Hide Logs";
-        } else {
-            logsdiv.style.display = "none";
-            logsbutton.innerHTML = "Show Logs";
-        }
-    }
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    theme.querySelector(".icon").innerHTML = "&#9789;";
 }
 
-function myoutput(nums) {
-    labelsall = [];
-    output.innerHTML = '<button onclick="showlogs()" id="showlogs" class="blue">Show Logs</button><div style="display:none;" id="logs"></div>';
-    for (let i = 0; i < nums.length; i++) {
-        labelsall.push(i);
+side.addEventListener("click", (e) => {
+    if (e.target.classList.contains("icon") || e.target.classList.contains("item")) {
+        let clicked = (e.target.classList.contains("item")) ? e.target.id: e.target.parentElement.id;
+        for ( let item of items ) {
+            if (item.id === clicked) {
+                if (item.classList.contains("hideChild")) item.classList.remove("hideChild");
+            } else {
+                if (!item.classList.contains("hideChild")) item.classList.add("hideChild");
+            }
+        }
+        if (clicked === "theme") {
+            if (document.body.classList.contains("dark")) {
+                document.body.classList.remove("dark");
+                localStorage.setItem("theme", "light");
+                theme.querySelector(".icon").innerHTML = "&#127774;";
+            } else {
+                document.body.classList.add("dark");
+                localStorage.setItem("theme", "dark");
+                theme.querySelector(".icon").innerHTML = "&#9789;";
+            }
+        }
     }
-    chartparent.innerHTML = '<canvas id="myChart"></canvas><button id="resetZoom">Reset Zoom</button>';
-    myChart = new Chart(
-        document.getElementById('myChart'),
-        {
-            type: 'line',
-            data: {
-                labels: labelsall,
-                datasets: [{
-                    label: 'Collatz Conjecture graph',
-                    backgroundColor: '#434fe6',
-                    borderColor: '#434fe6',
-                    data: nums
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        grid: {
-                            color: 'grey',
-                            borderColor: 'grey'
-                        },
-                        ticks: {
-                            color: 'grey'
-                        }
-                    },
-                    x: {
-                        grid: {
-                            color: 'grey',
-                            borderColor: 'grey'
-                        },
-                        ticks: {
-                            color: 'grey'
-                        }
-                    }
+});
+
+const solve = document.getElementById("solve");
+const input = document.querySelector("input");
+const output = document.getElementById("output");
+const error = document.getElementById("error");
+const resetZoom = document.getElementById("resetZoom");
+let myChart = null;
+
+const solveX = () => {
+    if (myChart) myChart.destroy();
+    myChart = null;
+    if (!input.value || parseInt(input.value) != input.value || input.value <= 0) {
+        if (error.classList.contains("hidden")) error.classList.remove("hidden");
+        if (!resetZoom.classList.contains("hidden")) resetZoom.classList.add("hidden");
+    } else {
+        if (!error.classList.contains("hidden")) error.classList.add("hidden");
+        if (resetZoom.classList.contains("hidden")) resetZoom.classList.remove("hidden");
+        let scaleX = [];
+        let scaleY = [];
+        let val = input.value;
+        let start = 0;
+        while ( val != 1 ) {
+            scaleY.push(start); start++;
+            scaleX.push(val);
+            if ( val % 2 ) { // odd
+                val = (val * 3) + 1;
+            } else { // even
+                val = val / 2;
+            }
+        }
+        scaleY.push(start); scaleX.push(1);
+        myChart = new Chart(
+            document.getElementById("myChart"),
+            {
+                type: "line",
+                data: {
+                    labels: scaleY,
+                    datasets: [{
+                        label: "Collatz Conjecture graph",
+                        backgroundColor: "#434fe6",
+                        borderColor: "#434fe6",
+                        data: scaleX
+                    }]
                 },
-                plugins: {
-                    legend: {
-                        onClick: false
+                options: {
+                    scales: {
+                        y: {
+                            grid: {
+                                color: "grey",
+                                borderColor: "grey"
+                            },
+                            ticks: {
+                                color: "grey"
+                            }
+                        },
+                        x: {
+                            grid: {
+                                color: "grey",
+                                borderColor: "grey"
+                            },
+                            ticks: {
+                                color: "grey"
+                            }
+                        }
                     },
-                    zoom: {
-                        pan: {
-                            enabled: true,
-                            mode: 'x'
+                    plugins: {
+                        legend: {
+                            onClick: false
                         },
                         zoom: {
-                            wheel: {
-                                enabled: true
+                            pan: {
+                                enabled: true,
+                                mode: "x"
                             },
-                            pinch: {
-                                enabled: true
-                            },
-                            mode: 'x'
+                            zoom: {
+                                wheel: {
+                                    enabled: true
+                                },
+                                pinch: {
+                                    enabled: true
+                                },
+                                mode: "x"
+                            }
                         }
                     }
                 }
             }
-        }
-    );
-    document.getElementById('resetZoom').setAttribute("onclick","myChart.resetZoom()");
-    if(!nums.includes(4)) {
-        nums = [4,2,1];
-    }
-    var logs = document.querySelector("#logs");
-    nums.forEach( num => {
-        if ( num % 2 ) {
-            var hold = (3*num)+1;
-            logs.innerHTML += '<p>' + num + ' is Odd (3 x '+ num + ') + 1 = <span style="color:red;">' + hold + '</span></p>';
-        } else {
-            var hold = num / 2;
-            if (num != 2) {
-                logs.innerHTML += '<p>' + num + ' is Even ' + num + ' / 2 = <span style="color:blue;">' + hold + '</span></p>';
-            } else {
-                logs.innerHTML += '<p><b style="color:blue;">&#10532;</b>&nbsp;&nbsp;&nbsp; ' + num + ' is Even ' + num + ' / 2 = <span style="color:blue;">' + hold + '</span> &nbsp;&nbsp;&nbsp;<b style="color:red;">&#10534;</b></p>';
-            }
-        }
-    });
-}
-
-function solve( x ) {
-    if (!isNaN(x) && parseInt(x) && x > 0) {
-        if (!all.length) {
-            all.push(x);
-        }
-        var x_is = 'even';
-        if ( x % 2 ) {
-            x_is = 'odd';
-        }
-        if (x_is == 'even') {
-            all.push(x/2);
-            solve(x/2);
-        } else {
-            if (x!=1) {
-                all.push((3*x)+1);
-                solve((3*x)+1);
-            } else {
-                myoutput(all);
-            }
-        }
-    } else {
-        output.innerHTML = 'x is not a number';
-        document.getElementById("chartparent").innerHTML = '<canvas id="myChart"></canvas>';
-    }
-}
-
-function display(tooltip) {
-    if (tooltip.id == "hint") {
-        quote.querySelector(".show").style.display = "none";
-    } else {
-        hint.querySelector(".show").style.display = "none";
-    }
-    let text = tooltip.querySelector(".show");
-    if ( text.style.display == "block" ) {
-        text.style.display = "none";
-    } else {
-        text.style.display = "block";
-    }
-}
-
-submit.addEventListener("click", function() {
-    all = [];
-    solve(input.value);
-});
-
-input.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-        all = [];
-        solve(input.value);
-    }
-});
-
-quote.addEventListener("click", function() {
-    display(quote);
-});
-
-hint.addEventListener("click", function() {
-    display(hint);
-});
-
-window.onload = function () {
-    if (MODE != 'light') {
-        document.body.style.backgroundColor = '#202124';
-        title.style.color = '#fff';
-        mode.innerHTML = '<p class="click">&#9789;</p>';
-        form.querySelector("div").style.backgroundColor = '#fff';
-        for (var i = 0; i < tooltips.length; i++) {
-            tooltips[i].style.backgroundColor = '#fff';
-            tooltips[i].style.color = '#000';
-            tooltips[0].querySelector("span").style.color = 'red';
-            document.styleSheets[0].cssRules[0].style.cssText = "color:#fff;";
-        }
+        );
+        resetZoom.setAttribute("onclick", "if (myChart) myChart.resetZoom()");
     }
 };
 
-document.getElementById("mode").addEventListener('click', function() {
-    quote.querySelector(".show").style.display = "none";
-    hint.querySelector(".show").style.display = "none";
-    if (MODE == 'light') {
-        document.body.style.backgroundColor = '#202124';
-        title.style.color = '#fff';
-        mode.innerHTML = '<p class="click">&#9789;</p>';
-        form.querySelector("div").style.backgroundColor = '#fff';
-        for (var i = 0; i < tooltips.length; i++) {
-            tooltips[i].style.backgroundColor = '#fff';
-            document.styleSheets[0].cssRules[0].style.cssText = "color:#fff;";
-            tooltips[i].style.color = '#000';
-            tooltips[0].querySelector("span").style.color = 'red';
-        }
-        localStorage.setItem('mode', 'dark');
-        MODE = 'dark';
-    } else {
-        document.body.style.backgroundColor = '#fff';
-        title.style.color = '#000';
-        mode.innerHTML = '<p class="click">&#127774;</p>';
-        form.querySelector("div").style.backgroundColor = 'grey';
-        document.styleSheets[0].cssRules[0].style.cssText = "color:#000;";
-        for (var i = 0; i < tooltips.length; i++) {
-            tooltips[i].style.backgroundColor = '#000';
-            tooltips[i].style.color = '#fff';
-            tooltips[0].querySelector("span").style.color = 'yellow';
-        }
-        localStorage.setItem('mode', 'light');
-        MODE = 'light';
-    }
-});
-
-function scroll() {
-    if ( document.body.scrollTop > 30 || document.documentElement.scrollTop > 30 ) {
-        scrollButton.style.display = "block";
-    } else {
-        scrollButton.style.display = "none";
-    }
-}
-
-scrollButton.addEventListener('click', function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
-window.onscroll = function() {
-    scroll();
-};
+solve.addEventListener("click", () => { solveX() });
+input.addEventListener("keypress", (e) => { if (e.key === "Enter") solveX() });
